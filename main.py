79eb080db5d98a3a5accacf84a912d053e12b333
axllasthero <Maxim11052006@gmail.com>
2021-04-03 19:53:31 +0700

    123

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import conf
 import discord
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.members = True
+
+client = discord.Client(intents=intents)
 
 
 
@@ -24,18 +27,33 @@ async def on_message(message):
 
     if message.channel.id == 825309119589122088:
         msg = None
+        
 
-
-
+        ctx = message.content.split(" ", maxsplit=1)
         if message.content == "/hello":
             msg = f'Hello, {message.author.name}. I am {client.user.name}'
         elif message.content == "/about_me":
             msg = f'Your name is {message.author.name} \nYour id is {message.author.id}\n{"Your nickname is " + message.author.nick if message.author.nick != None else ""}\nAnd ur mom gay '
-        elif message.content == "/repeat":
-            msg = f'{message.content}'
+        elif message.content.startswith('/repeat'):
+            msg = message.content.replace('/repeat', '')
+        elif message.content.startswith('/get_member'):
+            mem_id = message.content.replace('/get_member', '')
+            if mem_id == '':
+                msg = f'Who?'
+            else:
+                msg = f'Name = {message.guild.get_member(825328952364761129)}'
+                print(mem_id.strip())
+        elif message.content.startswith('/members'):
+            msg = ''
+            count = 1
+            for member in message.guild.members:
+                
+                
+                msg += f'{count}. {member} {"- " + member.nick if member.nick != None else ""} - {member.id}\n'
+                count += 1
         await message.channel.send(msg)
 
-
+        
 
 
 client.run(conf.bot_token)
